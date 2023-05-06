abfa27f39b423620f8cac71b3b659a873666d05a
Matt-Seath <100132940+Matt-Seath@users.noreply.github.com>
2023-05-06 22:04:16 +1000

    Created HomeView and MainViewModel

---
+++ AutoMate/MVVM/ViewModel/MainViewModel.cs
@@ -0,0 +1,14 @@
+﻿using AutoMate.Core;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AutoMate.MVVM.ViewModel
+{
+    class MainViewModel : ObservableObject
+    {
+        public MainViewModel() { }
+    }
+}
