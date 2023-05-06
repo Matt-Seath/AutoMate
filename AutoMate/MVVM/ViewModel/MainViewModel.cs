c4cd2ce1526136327918a43fb2fe254cb4fd19fd
Matt-Seath <100132940+Matt-Seath@users.noreply.github.com>
2023-05-07 09:52:11 +1000

    Added Settings View and ViewModel

--- AutoMate/MVVM/ViewModel/MainViewModel.cs
+++ AutoMate/MVVM/ViewModel/MainViewModel.cs
@@ -9,6 +9,39 @@ namespace AutoMate.MVVM.ViewModel
 {
     class MainViewModel : ObservableObject
     {
-        public MainViewModel() { }
+        public RelayCommand HomeViewCommand { get; set; }
+
+        public RelayCommand SettingsViewCommand { get; set; }
+
+        public HomeViewModel HomeVm { get; set; }
+        public SettingsViewModel SettingsVm { get; set; }
+
+        private object _currentView;
+
+		public object CurrentView
+		{
+			get { return _currentView; }
+			set { _currentView = value;
+				OnPropertyChanged();
+			}
+		}
+
+
+		public MainViewModel()
+		{
+			HomeVm = new HomeViewModel();
+			SettingsVm = new SettingsViewModel();
+			CurrentView = HomeVm;
+
+			HomeViewCommand = new RelayCommand(o =>
+			{
+				CurrentView = HomeVm;
+			});
+
+            SettingsViewCommand = new RelayCommand(o =>
+            {
+                CurrentView = SettingsVm;
+            });
+        }
     }
 }
