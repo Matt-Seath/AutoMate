daeea5df65f3529601cafb869754ffd609a13780
Matt-Seath <100132940+Matt-Seath@users.noreply.github.com>
2023-05-06 20:55:27 +1000

    Added RelayCommand class

--- AutoMate/Core/ObservableObject.cs
+++ AutoMate/Core/ObservableObject.cs
@@ -1,12 +1,15 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.ComponentModel;
+using System.Runtime.CompilerServices;
 
 namespace AutoMate.Core
 {
-    class ObservableObject
+    class ObservableObject : INotifyPropertyChanged
     {
+        public event PropertyChangedEventHandler? PropertyChanged;
+
+        protected void OnPropertyChanged([CallerMemberName] string name = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(name));
+        }
     }
 }
