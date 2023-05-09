0b87fa7345fa055797ef38090629c0604a7a3ae8
Matt-Seath <100132940+Matt-Seath@users.noreply.github.com>
2023-05-09 21:57:49 +1000

    added AiServices.cs

--- AutoMate/Services/AiService.cs
+++ AutoMate/Services/AiService.cs
@@ -4,9 +4,9 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace AutoMate.Core
+namespace AutoMate.Services
 {
-    class AiServices
+    class AiService
     {
     }
 }
