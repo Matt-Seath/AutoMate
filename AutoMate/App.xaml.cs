6ffea39b114d696fcc184ef2cf6ff4d43cbab8ed
Matt-Seath <100132940+Matt-Seath@users.noreply.github.com>
2023-05-09 22:49:05 +1000

    Created apphost

--- AutoMate/App.xaml.cs
+++ AutoMate/App.xaml.cs
@@ -1,21 +1,40 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Configuration;
-using System.Data;
-using System.Linq;
-using System.Threading.Tasks;
-using System.Windows;
+﻿using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
 using Rystem.OpenAi;
-using Microsoft.Extensions.DependencyInjection;
+using System.Configuration;
+using System.Windows;
 
-namespace AutoMate
+namespace AutoMate;
+
+public partial class App : Application
 {
-    /// <summary>
-    /// Interaction logic for App.xaml
-    /// </summary>
-    public partial class App : Application
-    {
-        public IServiceProvider ServiceProvider { get; private set; }
+    public static IHost? AppHost { get; private set; }
 
+    public App()
+    {
+        AppHost = Host.CreateDefaultBuilder()
+            .ConfigureServices((hostContext, services) =>
+            {
+                services.AddSingleton<MainWindow>();
+            })
+            .Build();
     }
+
+    protected override async void OnStartup(StartupEventArgs e)
+    {
+        await AppHost!.StartAsync();
+
+        var startupForm = AppHost.Services.GetRequiredService<MainWindow>();
+        startupForm.Show();
+            
+        base.OnStartup(e);
+    }
+
+    protected override async void OnExit(ExitEventArgs e)
+    {
+        await AppHost!.StopAsync();
+        base.OnExit(e);
+    }
+
 }
+
